82d08cec8286fd0d9369407a95fba169960bfba8
CarlosCRG19 <carloscrg19@gmail.com>
2023-04-19 20:25:29 -0600

    Create application service ports

---
+++ app/application/ports/user_repository.py
@@ -0,0 +1,12 @@
+from app.domain.user import User
+
+
+class UserRepository:
+    def save(self, user: User) -> User:
+        pass
+
+    def get_by_id(self, user_id: int) -> User:
+        pass
+
+    def get_by_email(self, email: str) -> User:
+        pass
